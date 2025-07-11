be0f976b8b60aad320dd7ab21bf43b50e3661c42
Chelsia Tong <chelsiatong@gmail.com>
2025-07-11 17:56:42 -0400

    new changes

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.scss';
+
+export default function Login() {
+    return (
+    <div className="login">
+        <div className="login__container">
+            <div className="login__sc">
+                <p className="login__names">STEPHON & CHELSIA</p>
+                <p className="login__announcement">ARE GETTING MARRIED!</p>
+            </div>
+        </div>
+    </div>
+    );
+}
