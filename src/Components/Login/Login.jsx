3325e59be41f344e54169571a914f329efebda7e
chelsia tong <chelsiatong@gmail.com>
2025-07-11 21:40:30 -0400

    inital changes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,22 +3,26 @@ import './Login.scss';
 
 export default function Login() {
     return (
-    <div className="login">
-        <div className="login__container">
-            <div className="login__sc">
-                <p className="login__names">Stephon  & Chelsia</p>
-                <p className="login__announcement">ARE GETTING MARRIED!</p>
+        <div className="login">
+            <div className="login__container">
+                <div className="login__sc">
+                    <p className="login__names">Stephon  & Chelsia</p>
+                    <p className="login__announcement">ARE GETTING MARRIED!</p>
+                </div>
+            </div>
+            <div className="login__box">
+                <div>
+                    <p className="login__heading">We're thrilled you're here!</p>
+                    <p className="login__description">Use the email where you received our invitation to log in.</p>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <div className="login__heading">
+                        <input />
+                        <button>Submit</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="login__box">
-            <div>
-                <p className="login__heading">We're thrilled you're here!</p>
-                <p className="login__description">Use the email where you received our invitation to log in.</p>
-            </div>
-            <div>
-                <p>Email</p>
-            </div>
-        </div>
-    </div>
     );
 }
