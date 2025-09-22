9361a64cccb678baed0144bbb9a8e6e1cab6885d
Chelsia Tong <chelsiatong@gmail.com>
2025-09-21 21:11:57 -0400

    did the mobile of email invite. def need to make it responsive. very rough. need to still link things, email and button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import './Home.scss';
+import React from "react";
+import "./Home.scss";
 
 export default function Home() {
-    return (
+  return (
     <div className="home">
-        <div className="home__welcome">
-            <p>Welcome, #name</p>
-        </div>
-        <div className="home__sc">
-            <p className="home__names">Stephon & Chelsia</p>
-            <p className="home__announcement">ARE GETTING MARRIED!</p>
-        </div>
-        <div className="home__location-time">
-            <p>NORTH YORK, ONTARIO <br/>SUNDAY, SEPTEMBER 6, 2026</p>
-        </div>
+      <div className="home__welcome">
+        <p>Welcome, #name</p>
+      </div>
+      <div className="home__sc">
+        <p className="home__names">Stephon & Chelsia</p>
+        <p className="home__announcement">ARE GETTING MARRIED!</p>
+      </div>
+      <div className="home__location-time">
+        <p>
+          NORTH YORK, ONTARIO <br />
+          SUNDAY, SEPTEMBER 6, 2026
+        </p>
+      </div>
     </div>
-    );
+  );
 }
