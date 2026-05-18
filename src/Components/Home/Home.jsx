87d21319c4aea7fd541956b63c4aeb5a1cf6abb2
Chelsia Tong <chelsiatong@gmail.com>
2026-05-17 21:48:56 -0400

    fixing homepage date/location

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -24,9 +24,22 @@ export default function Home() {
           </p>
         )}
       </div>
-      <div className="home__sc">
-        <p className="home__names">Stephon & Chelsia</p>
-        <p className="home__announcement">ARE GETTING MARRIED!</p>
+      <div>
+        <div className="home__sc">
+          <p className="home__names">Stephon & Chelsia</p>
+          <p className="home__announcement">ARE GETTING MARRIED!</p>
+        </div>
+        {isLoggedIn && (
+          <div className="home__location-time">
+            <p>
+              <a href="http://google.com/maps/place/Village+Loft/@43.7678421,-79.3879562,17z/data=!3m1!4b1!4m6!3m5!1s0x882b2d028bc2ef35:0x45b75cbbe45febef!8m2!3d43.7678383!4d-79.3853813!16s%2Fg%2F11f7bqwqh9?entry=ttu&g_ep=EgoyMDI2MDUxMy4wIKXMDSoASAFQAw%3D%3D">
+                2901 BAYVIEW AVE, NORTH YORK
+              </a>
+              <br />
+              SUNDAY, SEPTEMBER 6, 2026
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
